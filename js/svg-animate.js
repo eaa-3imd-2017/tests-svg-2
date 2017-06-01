55e6d02b66e3541c365bee43cf2c56cbf66e6ebf
Manuel Schmalstieg <git.code@ms-studio.net>
2017-06-02 01:28:49 +0200

    testing

--- js/svg-animate.js
+++ js/svg-animate.js
@@ -45,7 +45,7 @@ svgExt.addEventListener("load",function() {
   svgUk.addEventListener("mouseleave", svgCountryHide);
    
   svgAllemagne.addEventListener("mouseenter", svgCountryInfo);
-  svgAllemagne.addEventListener("click", svgCountryInfo);
+  // svgAllemagne.addEventListener("click", svgCountryInfo);
   svgAllemagne.addEventListener("mouseleave", svgCountryHide);
 
  }, false);
